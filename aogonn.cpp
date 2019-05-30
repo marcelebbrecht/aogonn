fb71356a0d14521100797c58ba44869ca90a808e
Marcel Ebbrecht <marcel.ebbrecht@googlemail.com>
2019-05-30 09:11:55 +0200

    build system created

---
+++ aogonn.cpp
@@ -0,0 +1,13 @@
+/*
+ * aogonn - main program
+ * copyright by Marcel Ebbrecht, 2019 <marcel.ebbrecht@googlemail.com>
+ */        
+        
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!";    
+    return 0;
+}
